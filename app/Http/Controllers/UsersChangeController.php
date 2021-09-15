fb14fe6f5233c9f140fe9076142acba142505c63
unknown <Abderhmanshallan@gmail.com>
2021-09-15 14:15:22 +0300

    update

--- app/Http/Controllers/UsersChangeController.php
+++ app/Http/Controllers/UsersChangeController.php
@@ -1,20 +1,15 @@
-
 <?php
-
 namespace App\Http\Controllers;
-
 use App\awards;
 use App\Models\User;
 use App\UserPoints;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 class UsersChangeController extends Controller
 {
     public function updateprofile(Request $request)
     {
         $id = Auth::user()->id;
-
         $this->validate(
             $request,
             [
@@ -28,17 +23,18 @@ class UsersChangeController extends Controller
         $data['number_id_pubg']              = $request->number_id_pubg;
         $data['number_id_freefire']          = $request->number_id_freefire;
         $data['number_id_googleplay']        = $request->number_id_googleplay;
-
- 
-
-        
+        if ($data['number_id_pubg']  ===  null)
+            unset($data['number_id_pubg']);
+        if ($data['number_id_googleplay']  ===  null)
+            unset($data['number_id_googleplay']);
+        if ($data['number_id_freefire']   ===  null)
+            unset($data['number_id_freefire']);
         $user = User::where('id', $id)->update($data);
         return response()->json([
             'user_updated' => true,
             'user' => $data
         ], 201);
     }
-
     public function getInformation()
     {
         $user = Auth::user();
@@ -51,7 +47,6 @@ class UsersChangeController extends Controller
             ]
         );
     }
-
     public function check_number_id($type)
     {
         $user = Auth::user();
@@ -68,6 +63,17 @@ class UsersChangeController extends Controller
             return response()->json(['type' => 'freefire', 'number_id_pubg' => $number_id_freefire,], 200);
         }
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
